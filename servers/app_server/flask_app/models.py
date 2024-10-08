7673c906bbc93e399d1a80e0fd902cd58c3e614d
Hhelibebcnofne <flyingboat256@gmail.com>
2024-10-08 09:56:35 +0900

    feat: add created_at and updated_at columns

--- servers/app_server/flask_app/models.py
+++ servers/app_server/flask_app/models.py
@@ -5,6 +5,10 @@ class User(db.Model):
     __tablename__ = "escalator-enquete"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), onupdate=db.func.now()
+    )
 
     def __init__(self, name):
         self.name = name
