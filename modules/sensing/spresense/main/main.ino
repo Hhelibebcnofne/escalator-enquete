55a546d31ff2252d2db5b207191ce50955e4cdd5
shimamoto <naoshi0308@outlook.jp>
2024-11-07 12:48:39 +0900

    カウントのロジックを追加

--- modules/sensing/spresense/main/main.ino
+++ modules/sensing/spresense/main/main.ino
@@ -10,7 +10,7 @@
 
 #define LR_INVERSION false
 #define LR_THRESHOLD 200
-#define WALL_THRESHOLD 1000
+#define WALL_THRESHOLD 500
 #define USE_HALF_WALL_THRESHOLD true
 #if USE_HALF_WALL_THRESHOLD
     #define LR_THRESHOLD
@@ -86,25 +86,41 @@ void start_bluetooth_process() {
 }
 
 void start_distance_sensor() {
-    while (true) {
+    unsigned long start = millis();
+    int lr_result = -1;
+    // int count_status = -1;
+    // while (millis() - start < SENSING_RATE_MS) {
+    while (true){
         uint16_t distance_value = tof_sensor.get_distance();
-        int lr_result = -1;
-        if (distance_value > LR_THRESHOLD && distance_value < WALL_THRESHOLD) {
-            lr_result = 1;
-        } else if (distance_value < LR_THRESHOLD) {
-            lr_result = 0;
-        }
         Serial.print("Distance: ");
         Serial.println(distance_value);
-        Serial.print("LR Result: ");
-        Serial.println(lr_result);
+        if (distance_value > LR_THRESHOLD && distance_value < WALL_THRESHOLD) {
+            if (lr_result == 1) {
+                continue;
+            }
+            lr_result = 1;
+            // count_status = 1;
+        } else if (distance_value < LR_THRESHOLD) {
+            if (lr_result == 0) {
+                continue;
+            }
+            lr_result = 0;
+            // count_status = 0;
+        } else {
+            lr_result = -1;
+            continue;
+        }
+
+        // Serial.print("LR Result: ");
+        // Serial.println(lr_result);
 
         {
             std::lock_guard<std::mutex> lock(queueMutex);
             sensorQueue.push(lr_result);  // センサデータをキューに追加
         }
         cv.notify_one();         // Bluetoothスレッドに通知
-        delay(SENSING_RATE_MS);  // センサデータの取得周期
+
+        // delay(SENSING_RATE_MS);  // センサデータの取得周期
     }
 }
 
@@ -172,11 +188,10 @@ bool get_question_mqtt_subscribe(WiFi_Module_Manager& wifi_manager) {
 }
 
 void loop() {
-    // ※短すぎるとエラーになるが、長いとERROR: Stack pointer is not within the
-    // stackになる delay(5000); // 切り替え間隔を調整
-    if (mqtt_flag) {
-        get_question_mqtt_subscribe(wifi_module_manager);
-        send_count_mqtt_publish(wifi_module_manager);
-        mqtt_flag = false;
-    }
+    // delay(5000); // 切り替え間隔を調整
+    // if (mqtt_flag) {
+    //     get_question_mqtt_subscribe(wifi_module_manager);
+    //     send_count_mqtt_publish(wifi_module_manager);
+    //     mqtt_flag = false;
+    // }
 }
