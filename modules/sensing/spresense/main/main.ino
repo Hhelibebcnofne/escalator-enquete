53acb9448039249d2fe63f1d4f147c99d2602c17
Hhelibebcnofne <flyingboat256@gmail.com>
2024-11-12 22:28:17 +0900

    feat: WiFiモジュール管理の追加とMQTT通信機能のサブコア実装

--- modules/sensing/spresense/main/main.ino
+++ modules/sensing/spresense/main/main.ino
@@ -1,5 +1,6 @@
 // 共通処理
 #include <MP.h>
+#define LR_INVERSION false
 
 enum class SensorResult {
     RightDetected = 1,  // 右判定
@@ -21,7 +22,7 @@ void setup() {
     MP.begin();             // SubCore起動完了通知
     BT.begin(BT_BAUDRATE);  // Bluetooth通信開始
 
-    MPLog("[SubCore1] Bluetooth communication started");
+    MPLog("[SubCore1] Bluetooth communication started\n");
 }
 
 void loop() {
@@ -63,6 +64,50 @@ void loop() {
 
 #elif (SUBCORE == 2)
 // SubCore2 ビルド
+#include "WiFi_Module_Manager.h"
+#define SUBSCRIBE_TIMEOUT 1000
+
+WiFi_Module_Manager wifi_module_manager;
+
+/* bool get_question_mqtt_subscribe(WiFi_Module_Manager& wifi_manager) {
+    if (wifi_manager.initMQTT()) {
+        Serial.println("MQTTに再接続しました (Subscribe モード)");
+        // Subscribe 処理
+        wifi_manager.mqttSubscribe();
+        return true;
+    } else {
+        Serial.println("MQTTの再接続に失敗しました。処理を停止します。");
+        return false;
+    }
+} */
+
+bool send_count_mqtt_publish(WiFi_Module_Manager& wifi_manager,
+                             String message) {
+    // // Publishモード時の再接続
+    if (wifi_manager.initMQTT()) {
+        Serial.println("MQTTに再接続しました (Publish モード)");
+        // // Publish 処理
+        wifi_manager.mqttPublish(message.c_str());  // MQTTでカウントを送信
+        return true;
+    } else {
+        Serial.println("MQTTの再接続に失敗しました。処理を停止します。");
+        return false;
+    }
+}
+void setup() {
+    MP.begin();
+    wifi_module_manager.setup();
+    MPLog("[SubCore2] Started\n");
+}
+
+void loop() {
+    String message;
+    if (MP.Recv(2, &message) == 2) {
+        send_count_mqtt_publish(wifi_module_manager, message.c_str());
+    } else {
+        wifi_module_manager.mqttSubscribe();
+    }
+}
 
 #elif (SUBCORE == 3)
 // SubCore3 ビルド
@@ -79,27 +124,23 @@ void loop() {
 #include <mutex>
 #include <queue>
 #include "ToF_Sensor.h"
-#include "WiFi_Module_Manager.h"
 
-#define LR_INVERSION false
 #define LR_THRESHOLD 200
 #define WALL_THRESHOLD 500
 #define USE_HALF_WALL_THRESHOLD true
 
 #if USE_HALF_WALL_THRESHOLD
+
 #define LR_THRESHOLD
 #define LR_THRESHOLD (int)(WALL_THRESHOLD / 2)
+
 #endif
 
 #define TIMER_INTERVAL_US 10000000
-#define SUBSCRIBE_TIMEOUT 1000
 #define CONSOLE_BAUDRATE 115200
 
 ToF_Sensor tof_sensor;
-WiFi_Module_Manager wifi_module_manager;
 
-std::queue<SensorResult> sensorQueue;
-std::mutex queueMutex;
 std::condition_variable cv;
 
 int left_count = 0;
@@ -110,6 +151,33 @@ std::mutex countMutex;
 bool mqtt_flag = false;
 
 pthread_t distance_sensor_process;
+
+unsigned int set_mqtt_flag() {
+    mqtt_flag = true;
+    return TIMER_INTERVAL_US;
+}
+
+void publish_mqtt_counts() {
+    String payload;
+    {
+        std::lock_guard<std::mutex> countLock(countMutex);
+        payload = "Left: " + String(left_count) +
+                  ", Right: " + String(right_count) +
+                  ", Errors: " + String(error_count);
+        // カウントをリセット
+        left_count = 0;
+        right_count = 0;
+        error_count = 0;
+    }
+    // wifi_manager.mqttPublish(payload.c_str());  // MQTTでカウントを送信
+    MP.Send(2, &payload, 2);
+}
+
+void initLED() {
+    pinMode(LED0, OUTPUT);
+    digitalWrite(LED0, LOW);  // LEDを消灯
+    Serial.println("LEDの初期化が完了し、消灯しました。");
+}
 
 void start_distance_sensor() {
     SensorResult lr_result =
@@ -135,86 +203,30 @@ void start_distance_sensor() {
             continue;
         }
 
-        {
-            std::lock_guard<std::mutex> lock(queueMutex);
-            sensorQueue.push(lr_result);
-        }
         MP.Send(1, static_cast<int>(lr_result), 1);
         // cv.notify_one();  // Bluetoothスレッドに通知
-    }
-}
-
-unsigned int set_mqtt_flag() {
-    mqtt_flag = true;
-    return TIMER_INTERVAL_US;
-}
-
-void publish_mqtt_counts(WiFi_Module_Manager& wifi_manager) {
-    String payload;
-    {
-        std::lock_guard<std::mutex> countLock(countMutex);
-        payload = "Left: " + String(left_count) +
-                  ", Right: " + String(right_count) +
-                  ", Errors: " + String(error_count);
-        // カウントをリセット
-        left_count = 0;
-        right_count = 0;
-        error_count = 0;
-    }
-    wifi_manager.mqttPublish(payload.c_str());  // MQTTでカウントを送信
-}
-
-void initLED() {
-    pinMode(LED0, OUTPUT);
-    digitalWrite(LED0, LOW);  // LEDを消灯
-    Serial.println("LEDの初期化が完了し、消灯しました。");
-}
-
-bool send_count_mqtt_publish(WiFi_Module_Manager& wifi_manager) {
-    // // Publishモード時の再接続
-    if (wifi_manager.initMQTT()) {
-        Serial.println("MQTTに再接続しました (Publish モード)");
-        // // Publish 処理
-        publish_mqtt_counts(wifi_manager);  // カウントデータをMQTTで送信
-        return true;
-    } else {
-        Serial.println("MQTTの再接続に失敗しました。処理を停止します。");
-        return false;
-    }
-}
-
-bool get_question_mqtt_subscribe(WiFi_Module_Manager& wifi_manager) {
-    if (wifi_manager.initMQTT()) {
-        Serial.println("MQTTに再接続しました (Subscribe モード)");
-        // Subscribe 処理
-        wifi_manager.mqttSubscribe();
-        return true;
-    } else {
-        Serial.println("MQTTの再接続に失敗しました。処理を停止します。");
-        return false;
     }
 }
 
 void setup() {
     Serial.begin(CONSOLE_BAUDRATE);  // PCとの通信
     initLED();
-    tof_sensor.setup();
-    // wifi_module_manager.setup();
+    // tof_sensor.setup();
 
     attachTimerInterrupt(set_mqtt_flag, TIMER_INTERVAL_US);
-    pthread_create(&distance_sensor_process, NULL, start_distance_sensor, NULL);
+    // pthread_create(&distance_sensor_process, NULL, start_distance_sensor,
+    // NULL);
     MP.begin(1);
+    MP.begin(2);
     Serial.println("MainCore: Started");
 }
 
 void loop() {
-    // Serial.println(tof_sensor.get_distance());
-    delay(5000);  // 切り替え間隔を調整
-    // if (mqtt_flag) {
-    //     get_question_mqtt_subscribe(wifi_module_manager);
-    //     send_count_mqtt_publish(wifi_module_manager);
-    //     mqtt_flag = false;
-    // }
+    delay(5000);
+    if (mqtt_flag) {
+        publish_mqtt_counts();
+        mqtt_flag = false;
+    }
 }
 
 #endif
