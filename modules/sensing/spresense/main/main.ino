10b5a6f8dde276b41357b41e296e4f652e1e48df
Hhelibebcnofne <flyingboat256@gmail.com>
2024-11-14 12:15:15 +0900

    feat: GS2200 Wi-FiおよびMQTTの初期化機能を追加し、メッセージ送受信処理を実装

--- modules/sensing/spresense/main/main.ino
+++ modules/sensing/spresense/main/main.ino
@@ -64,42 +64,146 @@ void loop() {
 
 #elif (SUBCORE == 2)
 // SubCore2 ビルド
-#include "WiFi_Module_Manager.h"
+#include <MqttGs2200.h>
+#include <TelitWiFi.h>
+#include "config.h"
+
 #define CONSOLE_BAUDRATE 115200
+#define SUBSCRIBE_TIMEOUT 2000
 
-WiFi_Module_Manager wifi_module_manager;
+TelitWiFi gs2200;
+TWIFI_Params gsparams;
+MqttGs2200 theMqttGs2200 = MqttGs2200(&gs2200);
 
-bool send_count_mqtt_publish(WiFi_Module_Manager& wifi_manager,
-                             String message) {
-    // Publishモード時の再接続
-    if (wifi_manager.initMQTT()) {
-        MPLog("MQTTに再接続しました (Publish モード)");
-        // Publish 処理
-        wifi_manager.mqttPublish(message.c_str());  // MQTTでカウントを送信
-        return true;
-    } else {
-        MPLog("MQTTの再接続に失敗しました。処理を停止します。");
+bool initWiFi() {
+    gsparams.mode = ATCMD_MODE_STATION;
+    gsparams.psave = ATCMD_PSAVE_DEFAULT;
+
+    Serial.println("Wi-Fiの初期化中...");
+    if (gs2200.begin(gsparams)) {
+        Serial.println("GS2200の初期化に失敗しました");
         return false;
     }
+    Serial.println("GS2200の初期化が成功しました。");
+
+    if (gs2200.activate_station(AP_SSID, PASSPHRASE)) {
+        Serial.println("Wi-Fiの接続に失敗しました");
+        return false;
+    }
+    Serial.println("SSID: " + String(AP_SSID) + " に接続しました");
+    return true;
 }
+
+bool initMQTT() {
+    MQTTGS2200_HostParams hostParams;
+    hostParams.host = (char*)MQTT_SRVR;
+    hostParams.port = (char*)MQTT_PORT;
+    hostParams.clientID = (char*)MQTT_CLI_ID;
+    hostParams.userName = (char*)MQTT_USERNAME;
+    hostParams.password = (char*)MQTT_PASSWPRD;
+
+    Serial.println("MQTTの初期化中...");
+    theMqttGs2200.begin(&hostParams);
+    if (!theMqttGs2200.connect()) {
+        Serial.println("MQTTの接続に失敗しました");
+        return false;
+    }
+    Serial.println("MQTTに接続しました。");
+    return true;
+}
+
+bool mqttPublish(char* send_message) {
+    Serial.println("MQTTメッセージの送信準備中");
+    WiFi_InitESCBuffer();
+    MQTTGS2200_Mqtt mqtt;
+
+    // Publishトピックを指定
+    strncpy(mqtt.params.topic, MQTT_PUBLISH_TOPIC, sizeof(mqtt.params.topic));
+    mqtt.params.QoS = 0;
+    mqtt.params.retain = 0;
+
+    // メッセージとして引数 `send_message` をセット
+    strncpy(mqtt.params.message, send_message, sizeof(mqtt.params.message));
+    mqtt.params.len = strlen(mqtt.params.message);
+
+    // メッセージ送信
+    if (theMqttGs2200.publish(&mqtt)) {
+        Serial.println("送信したメッセージ: " + String(send_message));
+    } else {
+        Serial.println("メッセージの送信に失敗しました: " +
+                       String(send_message));
+        return false;
+    }
+    theMqttGs2200.stop();
+    return true;
+}
+
+bool mqttSubscribe() {
+    Serial.println("MQTTメッセージの受信準備中");
+    MQTTGS2200_Mqtt mqtt;
+
+    // Subscribeトピックを指定
+    strncpy(mqtt.params.topic, MQTT_SUBSCRIBE_TOPIC, sizeof(mqtt.params.topic));
+    mqtt.params.QoS = 0;
+    mqtt.params.retain = 0;
+
+    if (theMqttGs2200.subscribe(&mqtt)) {
+        Serial.println("/mqtt/spresense/subscribe に購読しました");
+    } else {
+        Serial.println("購読に失敗しました");
+        return false;
+    }
+
+    unsigned long start = millis();
+    while (millis() - start < SUBSCRIBE_TIMEOUT) {
+        String data;
+        while (gs2200.available()) {
+            if (!theMqttGs2200.receive(data)) {
+                Serial.println("データの受信に失敗しました。");
+            }
+            Serial.println("受信データ: " + data);
+        }
+    }
+
+    Serial.printf("購読タイムアウト: %d ms経過しました！\n", SUBSCRIBE_TIMEOUT);
+    theMqttGs2200.stop();
+    return true;
+}
+
 void setup() {
     MP.begin();
     Serial.begin(CONSOLE_BAUDRATE);  // PCとの通信
     MPLog("[SubCore2] WiFi Setup\n");
-    wifi_module_manager.setup();
+    Init_GS2200_SPI_type(iS110B_TypeC);  // GS2200のSPI初期化
+    // initLED(); // LEDの初期化
+
+    if (!initWiFi()) {
+        Serial.println("Wi-Fiの初期化に失敗しました。処理を停止します。");
+        while (1)
+            ;
+    }
+
+    if (!initMQTT()) {
+        Serial.println("MQTTの初期化に失敗しました。処理を停止します。");
+        while (1)
+            ;
+    }
+
+    digitalWrite(LED0, HIGH);  // LEDを点灯
+    Serial.println("LEDを点灯しました。");
     MPLog("[SubCore2] Started\n");
 }
 
 void loop() {
-    // String message;
-    // if (MP.Recv(2, &message) == 2) {
-    //     MPLog("[SubCore2] Received data: %s\n", message.c_str());
-    //     MPLog("[SubCore2] MQTT Publish\n");
-    //     send_count_mqtt_publish(wifi_module_manager, message.c_str());
-    // } else {
-    //     wifi_module_manager.mqttSubscribe();
-    // }
-    // delay(5000)
+    String message;
+    if (MP.Recv(2, &message) == 2) {
+        MPLog("[SubCore2] Received data: %s\n", message.c_str());
+        mqttPublish((char*)message.c_str());
+        MPLog("[SubCore2] MQTT Publish\n");
+    } else {
+        MPLog("[SubCore2] No data received\n");
+    }
+    delay(5000);
 }
 
 #elif (SUBCORE == 3)
@@ -213,10 +317,10 @@ void setup() {
 }
 
 void loop() {
-    // delay(40000);
+    delay(40000);
     // if (mqtt_flag) {
-    // publish_mqtt_counts();
-    // mqtt_flag = false;
+    publish_mqtt_counts();
+    mqtt_flag = false;
     // }
 }
 
